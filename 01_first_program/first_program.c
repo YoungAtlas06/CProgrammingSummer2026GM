09620a4959eb64f6d8b93642861cef903016f5cb
YoungAtlas06 <gmontero0413@floridapoly.edu>
2026-05-20 18:16:49 +0000

    Create first Program

---
+++ 01_first_program/first_program.c
@@ -0,0 +1,23 @@
+/*
+
+*/
+
+
+
+/* multiline comment*/
+
+//one-line comment
+
+
+//Preprocessor directive (starts with#)
+//stdio - standard input output
+//.h - header file
+#include <stdio.h>
+
+// create the main function
+//<return type> <name> (<paremeters>)
+int main(void){
+    printf("Your\n MOM\n"); // \n - new line (escape character)
+    printf("Are\n you\n ready\n to\n code?\n");
+    return 0;
+}
